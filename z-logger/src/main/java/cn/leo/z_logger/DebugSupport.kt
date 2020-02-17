b63fcd308311beb41b30e42cf9e70470c491f6de
lingluo <lingluo511@gmail.com>
2020-02-17 14:37:25 +0800

    修复获取debug状态错误问题

--- z-logger/src/main/java/cn/leo/z_logger/DebugSupport.kt
+++ z-logger/src/main/java/cn/leo/z_logger/DebugSupport.kt
@@ -1,25 +1,25 @@
 package cn.leo.z_logger
 
+import android.content.Context
+import android.content.pm.ApplicationInfo
+
 /**
  * @author : ling luo
  * @date : 2020-02-11
  */
 
-internal fun isDebug() = BuildConfig.DEBUG
-
-/**
- * 测试代码段，只有在debug下会执行
- */
-internal inline fun debug(block: () -> Unit) {
-    if (isDebug()) {
-        block()
+internal fun isDebug(context: Context): Boolean {
+    return try {
+        context.applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE != 0
+    } catch (e: Exception) {
+        false
     }
 }
 
 /**
  * 测量代码块运行时间
  */
-internal inline fun measureTimeMillis(block: () -> Unit): Long {
+inline fun measureTimeMillis(block: () -> Unit): Long {
     val start = System.currentTimeMillis()
     block()
     return System.currentTimeMillis() - start
